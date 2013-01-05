b3777119a136e57d5f5c81a6039bccb6e0b0bbe4
Nathan Hwang <thenoviceoof@gmail.com>
2013-01-04 20:43:11 -0500

    Add a nearest neighbor heuristic solver
    Single core (both algos are single core at the moment), chose N**3 by
    default because otherwise seems silly.
    No test yet

--- tsp/tsp.go
+++ tsp/tsp.go
@@ -93,6 +93,7 @@ func permute_indicies(inds []int) []int {
 	return inds
 }
 
+// brute force: list all possible permutations of the points
 func brute_force_tsp(points [][]float64) [][]float64 {
 	// make a list of indicies to permute
 	inds := make([]int, len(points))
@@ -127,12 +128,67 @@ func brute_force_tsp(points [][]float64) [][]float64 {
 	return cur_points
 }
 
+// use the nearest neighbor heuristic
+func nearest_neighbor_tsp(points [][]float64) [][]float64 {
+	var min_pts [][]float64
+	var min_len float64 = math.Inf(1)
+	// for each point as starting point, run the NN heuristic
+	for i := 0; i < len(points); i++ {
+		var cur_len float64 = 0
+		cur_pts := make([][]float64, 1, len(points))
+		set_pts := make([][]float64, len(points))
+		// make a copy which we remove from, remove starting point
+		copy(set_pts, points)
+		cur_pt := set_pts[i]
+		cur_pts[0] = cur_pt
+		set_pts[i] = set_pts[len(set_pts) - 1]
+		set_pts = set_pts[:len(set_pts) - 1]
+		// find the shortest path
+		for len(set_pts) > 0 {
+			next_i := -1
+			var next_pt []float64
+			var next_dist float64 = math.Inf(1)
+			// find next shortest point
+			for i, p := range set_pts {
+				d := dist(cur_pt, p)
+				if d < next_dist {
+					next_pt = p
+					next_dist = d
+					next_i = i
+				}
+			}
+			cur_len += next_dist
+			cur_pt = next_pt
+			cur_pts = cur_pts[:len(cur_pts) + 1]
+			cur_pts[len(cur_pts) - 1] = next_pt
+			// remove the point
+			set_pts[next_i] = set_pts[len(set_pts) - 1]
+			set_pts = set_pts[:len(set_pts) - 1]
+		}
+		// check if it's smaller or not
+		if cur_len < min_len {
+			min_len = cur_len
+			min_pts = cur_pts
+		}
+	}
+	return min_pts
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Runtime things
 
 // algo types
-var brute_flag = flag.Bool("brute", false, `Brute force searcher
-	Time: O(n!) Space: O(n)`)
+var flag_algo_brute = flag.Bool("brute", false,
+	`Brute force searcher: slow and obvious
+	 Time: O(n!) Space: O(n)
+		Guaranteed to be correct`)
+var flag_algo_nn = flag.Bool("nearest-neighbor", false,
+	`Nearest Neighbor heuristic: fast and approximate
+	 Time: O(n**3) Space: O(n)
+		Path produced is on average 25% longer than optimal:
+	 [Johnson, D.S. and McGeoch, L.A.. "The traveling salesman problem:
+	 A case study in local optimization", Local search in combinatorial
+	 optimization, 1997, 215-310]`)
 
 func main() {
 	// parse the command line options
@@ -150,8 +206,10 @@ func main() {
 	}
 	// solve it
 	switch {
-	case *brute_flag:
+	case *flag_algo_brute:
 		points = brute_force_tsp(points)
+	case *flag_algo_nn:
+		points = nearest_neighbor_tsp(points)
 	default:
 		fmt.Fprintln(os.Stderr, "No algorithm selected.")
 		os.Exit(2)

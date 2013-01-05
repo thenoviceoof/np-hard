1946f01c6b84645b3cdfd9e83fcf0002baeb72be
Nathan Hwang <thenoviceoof@gmail.com>
2013-01-04 21:58:47 -0500

    Move some things into a np-hard/utils package
    Also: burn in hell for using a relative import

--- tsp/tsp.go
+++ tsp/tsp.go
@@ -13,94 +13,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"math"
 	"os"
+	"../utils"
 )
 
 ////////////////////////////////////////////////////////////////////////////////
-// UTILITIES
-
-// this will probably eventually go in a utility file
-func read_points_from_file(file io.Reader) ([][]float64, error) {
-	var x, y float64
-	points := make([][]float64, 0, 100)
-	for i := 0;; i++ {
-		// read pairs of floats for now
-		n, err := fmt.Fscanf(file, "%f %f", &x, &y)
-		if err == io.EOF {
-			break
-		}
-		if err != nil || n != 2 {
-			return nil, err
-		}
-		// check the capacity
-		if len(points) == cap(points) {
-			more_points := make([][]float64, len(points), 2*cap(points))
-			copy(more_points, points)
-			points = more_points
-		}
-		points = points[:i+1]
-		points[i] = []float64{x, y}
-	}
-	return points, nil
-}
-
-func read_points_from_stdin() ([][]float64, error) {
-	return read_points_from_file(os.Stdin)
-}
-
-////////////////////////////////////////////////////////////////////////////////
 // ALGOS
-
-// takes a pair of N-dim points, outputs the euclidean distance
-func dist(x, y []float64) float64 {
-	var sum float64 = 0
-	for i, _ := range x {
-		sum += math.Pow(x[i] - y[i], 2)
-	}
-	return math.Sqrt(sum)
-}
-
-// go through a path, add up all the distances between the points sequentially
-func path_dist(points [][]float64) float64 {
-	var cur_len float64 = 0
-	for i := 0; i < len(points) - 1; i++ {
-		cur_len += dist(points[i], points[i+1])
-	}
-	return cur_len
-}
-
-// from http://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order
-func permute_indicies(inds []int) []int {
-	var k, l int
-	// find the largest k a[k] < a[k+1]; none means done
-	for k = len(inds) - 2; k >= 0; k-- {
-		if inds[k] < inds[k + 1] {
-			break
-		}
-	}
-	if k == -1 {
-		return nil
-	}
-	// find largest l a[k] < a[l]
-	for l = len(inds) - 1; k < l ; l-- {
-		if inds[k] < inds[l] {
-			break
-		}
-	}
-	// swap k/l
-	tmp := inds[k]
-	inds[k] = inds[l]
-	inds[l] = tmp
-	// reverse a[k+1] until end
-	for j := 0; j < (len(inds) - k + 1)/2; j++ {
-		tmp = inds[k+1 + j]
-		inds[k+1 + j] = inds[len(inds) - 1 - j]
-		inds[len(inds) - 1 - j] = tmp
-	}
-	return inds
-}
 
 // brute force: list all possible permutations of the points
 func brute_force_tsp(points [][]float64) [][]float64 {
@@ -116,14 +35,14 @@ func brute_force_tsp(points [][]float64) [][]float64 {
 		// go through, add up the lengths
 		var cur_len float64 = 0
 		for i := 0; i < len(inds) - 1; i++ {
-			cur_len += dist(points[inds[i]], points[inds[i+1]])
+			cur_len += utils.Dist(points[inds[i]], points[inds[i+1]])
 		}
 		if cur_len < min_len {
 			min_len = cur_len
 			copy(min_inds, inds)
 		}
 		// permute
-		inds := permute_indicies(inds)
+		inds := utils.PermuteIndicies(inds)
 		// check if we're done
 		if inds == nil {
 			break
@@ -159,7 +78,7 @@ func nearest_neighbor_tsp(points [][]float64) [][]float64 {
 			var next_dist float64 = math.Inf(1)
 			// find next shortest point
 			for i, p := range set_pts {
-				d := dist(cur_pt, p)
+				d := utils.Dist(cur_pt, p)
 				if d < next_dist {
 					next_pt = p
 					next_dist = d
@@ -204,7 +123,7 @@ func main() {
 	flag.Parse()
 
 	// get the list of points (maybe also a graph)
-	points, err := read_points_from_stdin()
+	points, err := utils.ReadPointsFromStdin()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
---
+++ utils/utils.go
@@ -0,0 +1,96 @@
+/* utils.go
+ * various utilities used by different np-hard citizens
+ */
+
+package utils
+
+import (
+	"fmt"
+	"io"
+	"math"
+	"os"
+)
+
+//------------------------------------------------------------------------------
+// IO utils
+
+func ReadPointsFromFile(file io.Reader) ([][]float64, error) {
+	var x, y float64
+	points := make([][]float64, 0, 100)
+	for i := 0;; i++ {
+		// read pairs of floats for now
+		n, err := fmt.Fscanf(file, "%f %f", &x, &y)
+		if err == io.EOF {
+			break
+		}
+		if err != nil || n != 2 {
+			return nil, err
+		}
+		// check the capacity
+		if len(points) == cap(points) {
+			more_points := make([][]float64, len(points), 2*cap(points))
+			copy(more_points, points)
+			points = more_points
+		}
+		points = points[:i+1]
+		points[i] = []float64{x, y}
+	}
+	return points, nil
+}
+
+func ReadPointsFromStdin() ([][]float64, error) {
+	return ReadPointsFromFile(os.Stdin)
+}
+
+//------------------------------------------------------------------------------
+// graph utils
+
+// takes a pair of N-dim points, outputs the euclidean distance
+func Dist(x, y []float64) float64 {
+	var sum float64 = 0
+	for i, _ := range x {
+		sum += math.Pow(x[i] - y[i], 2)
+	}
+	return math.Sqrt(sum)
+}
+
+// NOT USED CURRENTLY
+// go through a path, add up all the distances between the points sequentially
+func path_dist(points [][]float64) float64 {
+	var cur_len float64 = 0
+	for i := 0; i < len(points) - 1; i++ {
+		cur_len += Dist(points[i], points[i+1])
+	}
+	return cur_len
+}
+
+// http://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order
+func PermuteIndicies(inds []int) []int {
+	var k, l int
+	// find the largest k a[k] < a[k+1]; none means done
+	for k = len(inds) - 2; k >= 0; k-- {
+		if inds[k] < inds[k + 1] {
+			break
+		}
+	}
+	if k == -1 {
+		return nil
+	}
+	// find largest l a[k] < a[l]
+	for l = len(inds) - 1; k < l ; l-- {
+		if inds[k] < inds[l] {
+			break
+		}
+	}
+	// swap k/l
+	tmp := inds[k]
+	inds[k] = inds[l]
+	inds[l] = tmp
+	// reverse a[k+1] until end
+	for j := 0; j < (len(inds) - k + 1)/2; j++ {
+		tmp = inds[k+1 + j]
+		inds[k+1 + j] = inds[len(inds) - 1 - j]
+		inds[len(inds) - 1 - j] = tmp
+	}
+	return inds
+}
